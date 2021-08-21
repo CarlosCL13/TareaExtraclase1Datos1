5f098ac74340a8e6d087cca8de1b9700f3a1a040
CarlosCL13 <carlos.andres.1c9l@gmail.com>
2021-08-20 23:09:59 -0600

    Inicio de las interfaces

--- TareaExtraclase1/src/Cliente.java
+++ TareaExtraclase1/src/Cliente.java
@@ -1,2 +1,65 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.*;
+
 public class Cliente {
+    /**
+     * @author Carlos Andrés Contreras Luna
+     * @param  args This is the class client and have all the methods to comunicate with the server
+     */
+
+    public static void main (String[] args){
+
+        InterfazCliente miinterfazc = new InterfazCliente();
+
+        miinterfazc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+}
+
+class InterfazCliente extends JFrame{
+
+    public InterfazCliente(){
+
+        setBounds(500,200,280,350);
+
+        ModeloInterfazCliente modelointerfazc = new ModeloInterfazCliente();
+
+        add(modelointerfazc);
+
+        setVisible(true);
+    }
+}
+
+class ModeloInterfazCliente extends JPanel {
+
+    public ModeloInterfazCliente(){
+
+        JLabel texto= new JLabel("Chat");
+
+        texto.setBounds(0,15, 10,10);
+
+        add(texto);
+
+        campochat = new JTextArea(12,22);
+
+        add(campochat);
+
+        campo1 = new JTextField(20);
+
+        add(campo1);
+
+        miboton=new JButton("Enviar");
+
+        add(miboton);
+    }
+
+    private JTextField campo1;
+
+    private JTextArea campochat;
+
+    private JButton miboton;
 }
--- TareaExtraclase1/src/Servidor.java
+++ TareaExtraclase1/src/Servidor.java
@@ -1,2 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.*;
+
 public class Servidor {
+
+    public static void main(String[] args){
+
+        InterfazServidor miinterfazs = new InterfazServidor();
+
+        miinterfazs.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    }
+}
+
+class InterfazServidor extends JFrame{
+
+    public InterfazServidor(){
+
+        setBounds(500,200,280,350);
+
+        JPanel mipanel = new JPanel();
+
+        mipanel.setLayout(new BorderLayout());
+
+        areatexto = new JTextArea();
+
+        mipanel.add(areatexto,BorderLayout.CENTER);
+
+        add(mipanel);
+
+        setVisible(true);
+
+
+
+    }
+
+    private	JTextArea areatexto;
 }
